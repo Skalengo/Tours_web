c2feb47b389a52e591e1a80122ef2b66e525ed97
Skalengo <93938074+Skalengo@users.noreply.github.com>
2021-11-21 00:28:35 -0800

    Add files via upload

--- AfricaKwetu/urls.py
+++ AfricaKwetu/urls.py
@@ -1,6 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.homeView, name="home"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(prefix=settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(prefix=settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
