be4545e8e0895602db309b373216954f844877b4
Skalengo <93938074+Skalengo@users.noreply.github.com>
2021-11-21 01:05:50 -0800

    Add files via upload

--- Tours/settings.py
+++ Tours/settings.py
@@ -126,6 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
